d7d598187beec68a20a50017d06ddf36865b6b58
tonmoy <tonmoy3673@gmail.com>
2023-10-11 01:41:31 +0600

    set fotter

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -50,6 +50,36 @@ const Home = () => {
           </ul>
         </div>
       </header>
+      {/* ============== Footer =================== */}
+      <footer className="dark:bg-gray-800 dark:text-gray-50">
+        <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-400">
+          <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
+            <li>Shop</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+          </ul>
+          <div className="flex flex-col justify-center pt-6 lg:pt-0">
+            <div className="flex justify-center space-x-4">
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                title="Instagram"
+                className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-400 dark:text-gray-900"
+              ></a>
+              <a
+                rel="noopener noreferrer"
+                href="#"
+                title="Pinterest"
+                className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-400 dark:text-gray-900"
+              ></a>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      {/* ============== end of homePage =========== */}
     </div>
   );
 };
