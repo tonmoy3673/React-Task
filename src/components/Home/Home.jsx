e45f9cb970b67c8be1eb16dff93dafcef6ecf687
tonmoy <tonmoy3673@gmail.com>
2023-10-11 03:13:56 +0600

    add sidebar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,79 +4,83 @@ const Home = () => {
       {/* ================= NavBar=================== */}
       <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
         <div className="container flex justify-between h-16 mx-auto">
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            aria-label="Back to homepage"
-            className="flex items-center p-2"
-          ></a>
           <ul className="items-stretch hidden space-x-3 lg:flex">
             <li className="flex">
               <a
                 rel="noopener noreferrer"
                 href="#"
-                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400"
+                className="flex items-center px-4 -mb-1"
               >
-                Mehedi Hasan Tonmoy
+                Name: Mehedi Hasan Tonmoy
               </a>
             </li>
             <li className="flex">
               <a
                 rel="noopener noreferrer"
                 href="#"
-                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+                className="flex items-center px-4 -mb-1"
               >
-                meheditonmoy917@gmail.com
+                Email: meheditonmoy917@gmail.com
               </a>
             </li>
             <li className="flex">
               <a
                 rel="noopener noreferrer"
                 href="#"
-                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+                className="flex items-center px-4 -mb-1"
               >
-                Khilkhet,Dhaka-1229
+                Address: Khilkhet,Dhaka-1229
               </a>
             </li>
             <li className="flex">
               <a
                 rel="noopener noreferrer"
                 href="#"
-                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+                className="flex items-center px-4 -mb-1"
               >
-                Phone:+8801966527486
+                Phone: +8801966527486
               </a>
             </li>
           </ul>
         </div>
       </header>
-      {/* ============== Footer =================== */}
-      <footer className="dark:bg-gray-800 dark:text-gray-50">
-        <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-400">
-          <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
-            <li>Shop</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Pricing</li>
-            <li>Contact</li>
+
+      {/* ===================== side bar ====================== */}
+      <div className="text-start py-12">
+        <div className="dropdown dropdown-right">
+          <label tabIndex={0} className="btn m-1">
+            React
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Counter</a>
+            </li>
+            <li>
+              <a>Form</a>
+            </li>
+            <li>
+              <a>List</a>
+            </li>
           </ul>
-          <div className="flex flex-col justify-center pt-6 lg:pt-0">
-            <div className="flex justify-center space-x-4">
-              <a
-                rel="noopener noreferrer"
-                href="#"
-                title="Instagram"
-                className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-400 dark:text-gray-900"
-              ></a>
-              <a
-                rel="noopener noreferrer"
-                href="#"
-                title="Pinterest"
-                className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-400 dark:text-gray-900"
-              ></a>
-            </div>
-          </div>
         </div>
+      </div>
+      {/* ============== Footer =================== */}
+      <footer className="footer p-10 bg-base-200 text-center">
+        <nav className="mx-auto">
+          <header className="footer-title">University</header>
+          <p>Branding</p>
+        </nav>
+        <nav>
+          <header className="footer-title">Subject</header>
+          <p>Computer Science Engineering</p>
+        </nav>
+        <nav>
+          <header className="footer-title">Passing Year</header>
+          <p>2023</p>
+        </nav>
       </footer>
 
       {/* ============== end of homePage =========== */}
